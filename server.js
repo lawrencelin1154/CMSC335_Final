01ca5996346aede96dae35b71a7e5915f0f7b5e6
Lawrence Lin <lawrencelin1154@gmail.com>
2026-05-05 14:31:08 -0400

    server fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api', require('./routes/api'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/dist')));
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist/index.html'));
   });
 }
